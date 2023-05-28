f73ee9fa16c03ab63716026cb1d182ef8b78e78f
Misha <zayzolkov11@gmail.com>
2023-05-28 17:29:31 +0300

    add changes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<nav>
+    <div class="nav-conitainer">
+        <a href="/" class="nav-log" title="Back To Homepage">Misha</a>
+        <div class="nav-links">
+            <a href="/about" class="link">About Me</a>
+            <a href="/about" class="link">Blog</a> 
+            <a href="/about" class="link">My Projects</a>
+            <a href="/about" class="link">Contacts</a>
+        </div>
+    </div>
+    
+</nav>
+<div class="container">
+    <slot></slot>
+</div>
+
+
+<style>
+    .container {
+        display: flex;
+        min-width: 1400px;
+        margin: 50px auto
+        ;
+    }
+
+</style>
