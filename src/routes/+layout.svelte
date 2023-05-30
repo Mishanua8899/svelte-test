3fd381d4e82f371cf2a035ab9b370cefc5d5c4b6
Misha <zayzolkov11@gmail.com>
2023-05-30 22:14:54 +0300

    add changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,45 @@
+<script>
+    const nav = [
+        {
+            title: "About me",
+            href: "/about",
+        },
+
+        {
+            title: "Blog",
+            href: "/blog",
+        },
+
+        {
+            title: "My projects",
+            href: "/myPrgct",
+        },
+
+        {
+            title: "Contacts",
+            href: "/contacts"  
+        },
+    ];
+</script>
+
 <nav>
     <div class="nav-conitainer">
         <a href="/" class="nav-log" title="Back To Homepage">Misha</a>
         <div class="nav-links">
-            <a href="/about" class="link">About Me</a>
-            <a href="/about" class="link">Blog</a> 
-            <a href="/about" class="link">My Projects</a>
-            <a href="/about" class="link">Contacts</a>
+           {#each nav as link}
+               <a href={link.href} class="link">{link.title}</a>
+           {/each}
         </div>
     </div>
-    
 </nav>
 <div class="container">
     <slot></slot>
 </div>
 
-
 <style>
     .container {
         display: flex;
         min-width: 1400px;
-        margin: 50px auto
-        ;
+        margin: 50px auto;
     }
-
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,7 @@
     <title>Misha</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
 
 
 <div class="container">
